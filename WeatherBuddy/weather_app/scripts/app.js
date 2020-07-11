7744601198fd8efe5fbe1a272a112ed0f97aca75
Arindam Baruah <arindambaruah4@gmail.com>
2020-07-11 18:59:59 +0530

    Add files via upload

---
+++ WeatherBuddy/weather_app/scripts/app.js
@@ -0,0 +1,60 @@
+const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
+
+const updateUI = (data) => {
+
+    const cityDets = data.cityDets;
+    const weather = data.weather;
+
+    //updating the template
+    details.innerHTML = `
+        <h5 class="my-3">${cityDets.EnglishName}</h5>
+        <div class="my-3">${weather.WeatherText}</div>
+        <div class="display-4 my-4">
+        <span>${weather.Temperature.Metric.Value}</span>
+        <span>&deg;C</span>
+        </div>
+    `
+    // update the night/day & icon images
+    const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+    icon.setAttribute('src', iconSrc);
+    
+    let timeSrc = null;
+    if(weather.IsDayTime){
+        timeSrc = 'img/day.svg';
+    } else {
+        timeSrc = 'img/night.svg';
+    }
+    time.setAttribute('src', timeSrc);
+
+    //remove d-none if present
+    if(card.classList.contains('d-none')){
+        card.classList.remove('d-none');
+    }
+}
+
+const updateCity = async (city) => {
+    
+    const cityDets = await getCity(city);
+    const weather = await getWeather(cityDets.Key);
+
+    return {
+        cityDets : cityDets,
+        weather : weather
+    };
+}
+
+cityForm.addEventListener('submit', (e) => {
+    //preventing default action
+    e.preventDefault();
+
+    //getting the city value
+    const city = cityForm.city.value.trim();
+    cityForm.reset();
+
+    //updating the UI
+    updateCity(city).then(data => updateUI(data)).catch(err => console.log(err));
+})
